31c1394e3a7f6e1bff9e1962a4bb1c1a593b58f6
caiosidesx <118694590+caiosidesx@users.noreply.github.com>
2026-07-14 20:27:01 -0300

    Adicionando função ao formulário e direcionando o usuário ao Whatsapp OBS: Concluído

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
+// FUNÇÃO PARA ABRIR E FECHAR O MENU
 const menuBtn = document.querySelector('.menu-icon');
 const sideBar = document.querySelector('.sidebar');
 
 
 function abrirMenu() {
-    menuBtn.addEventListener('click', (event) => {
+    menuBtn.addEventListener('click', (e) => {
         sideBar.classList.toggle('active');
         sideBar.style.left = '0';
 
-        sideBar.addEventListener('click', (event) => {
+        sideBar.addEventListener('click', (e) => {
             sideBar.classList.remove('active');
             sideBar.style.left = '-200px';
         })
     })
 }
+
+// FUNÇÃO PARA ENVIAR MENSAGEM PARA O WHATSAPP
+
+function enviarWhatsapp(event) {
+ event.preventDefault()
+
+const nome = document.getElementById('nome');
+const mensagem = document.getElementById('mensagem');
+const telefone = '5579998634165';
+
+const msgFormatada = `Olá, meu nome é ${nome.value}, ${mensagem.value}`;
+
+const url = `https://wa.me/${telefone}?text=${msgFormatada}`;
+
+window.open(url, "_blank")
+
+}
+
