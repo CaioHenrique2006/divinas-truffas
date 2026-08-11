2f95852dde70a4e4cb9fa335d93de637442910eb
caiosidesx <118694590+caiosidesx@users.noreply.github.com>
2026-08-11 09:26:31 -0300

    Bugs nos eventos de "clicks" corrigidos

--- script.js
+++ script.js
@@ -3,23 +3,18 @@ const menuBtn = document.querySelector('.menu-icon');
 const sideBar = document.querySelector('.sidebar');
 const overlay = document.querySelector('.overlay');
 
+menuBtn.addEventListener('click', abrirMenu);
+
 
 function abrirMenu() {
-    menuBtn.addEventListener('click', () => {
-        sideBar.classList.toggle('active');
-        overlay.classList.toggle('active');
-    });
-
-    sideBar.addEventListener('click', () => {
-        sideBar.classList.remove('active');
-        overlay.classList.remove('active');
-    })
-
-    overlay.addEventListener('click', () => {
-        sideBar.classList.remove('active');
-        overlay.classList.remove('active');
-    })
+    sideBar.classList.toggle('active');
+    overlay.classList.toggle('active');
 }
+
+overlay.addEventListener('click', () => {
+    sideBar.classList.remove('active');
+    overlay.classList.remove('active');
+})
 
 // FUNÇÃO PARA ENVIAR MENSAGEM PARA O WHATSAPP
 
